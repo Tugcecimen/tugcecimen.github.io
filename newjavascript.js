82871b1b4f13e44771d5319839d4043602a76595
tugcecimen <tugcecimen@std.iyte.edu.tr>
2016-03-18 10:30:14 +0200

    javascripts files 18 march lesson

---
+++ newjavascript.js
@@ -0,0 +1,21 @@
+var=timer
+var elemID = "#disp";
+
+
+
+$().ready(function() {
+    var left;
+    var top;
+    for(var i=0; i<10 ; i++) {
+        left= Math.floor((Math.random()*1000)+1);
+        top= Math.floor((Math.random()*800)+1);
+        $(elemID).append(
+           "<div id='" + i + "' class='b' style='top:"
+   + top + "px;left :" + left+ "px;'>"+ i + "</div>");
+   
+   $("#5").css("left",left+"px");
+   $("#5").css("top",top+"px");
+   $("#5").html(id++);          
+        },2000);
+    });
+ 
