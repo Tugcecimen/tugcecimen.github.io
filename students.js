7cf41e76c74a0d20a3fff7f93207d8cfc7cdbdd6
tugcecimen <tugcecimen@std.iyte.edu.tr>
2016-03-25 10:04:19 +0200

    25 march lesson

--- students.js
+++ students.js
@@ -1,4 +1,5 @@
-var Student = {
+
+   var Student = {
     'ID': '',
     'fName': '',
        'I name': '',
@@ -18,13 +19,24 @@ var Student = {
     }
     };
     var s1 = Student;
-    s1['ID']= "210104003";
-    s1['IName'] ="last";
-    s1['fName'] = "first";
+    s1['0']= "210104003"; //doesnt work ... will add a new variable to our model
+    s1['IName'] ="last"; // works as expected
+    s1['fName'] = "first"; // works as expected.
     for(var i=0; i<5 ; i++){
         s1['courses'][i]= {'courseID' :'c'+i, 'grade':Math.random()*100+1}
      }
+     s1['tel'] = "1314561531";
    var gpa =s1.calcGPA();
    
+   function Person(first,last,age,eye){
+       this.firstName = first;
+       this.lastName = last;
+       this.age = age;
+       this.eyeColor =eye;
+    
            
+   };
+   
+   var p1 =new Person ('first','last',112,'yellow');
+   var p2 = new Person ('first','last',12,'green');           
    
